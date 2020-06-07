acd51b397a3e19f709c179d1e23eb00b0c3c7341
Devang Jhabakh Jai <devangjhabakh@berkeley.edu>
2020-06-07 18:35:17 +0400

    Updates

---
+++ precache-manifest.bd580df36651fb6a5336e5a335f98987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e116f467bc19c9689a91a354cd587a7",
+    "url": "/react-smartbrain/index.html"
+  },
+  {
+    "revision": "742e63e8b99003c100f6",
+    "url": "/react-smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "23e9f4024302a5088b1f",
+    "url": "/react-smartbrain/static/css/main.031bd9cc.chunk.css"
+  },
+  {
+    "revision": "742e63e8b99003c100f6",
+    "url": "/react-smartbrain/static/js/2.a86d8c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-smartbrain/static/js/2.a86d8c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e9f4024302a5088b1f",
+    "url": "/react-smartbrain/static/js/main.36ff51e2.chunk.js"
+  },
+  {
+    "revision": "281d85e0591a2314ccad",
+    "url": "/react-smartbrain/static/js/runtime-main.487cd5a8.js"
+  },
+  {
+    "revision": "e772f35df3494a322ac7c65a2071c3c8",
+    "url": "/react-smartbrain/static/media/brain.e772f35d.png"
+  }
+]);
